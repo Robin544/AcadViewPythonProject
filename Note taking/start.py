68783c6a4e90291f56d6cee41481c2fa8c9a5102
Robin Singh <robinsinghkamboj@gmail.com>
2018-07-07 12:09:15 +0530

    my training project in Python

---
+++ Note taking/start.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox
+
+from NoteDB import NoteDB
+from dashboard import Dashboard
+if __name__ == "__main__":
+    try:
+        # Add Your Database username and password here
+        db = NoteDB(username="root", password="newrootpassword")
+        Dashboard().initUI(db)
+    except Exception as e:
+        messagebox.showinfo("Error", "Unable to establish database connection!")
